9e209ac4e1a8e867d58a2a852002932749801012
Vikash Gupta <vikashg2512@gmail.com>
2022-04-27 19:22:17 +0530

    update

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from docx import Document
+from docx.shared import Inches
 
 document = Document()
 
+# profile picture
+document.add_picture('profile.png',width=Inches(1.25))
+
+# name phone number and email details
 name = input('name? ')
 phone_number = input('phone number? ')
 email = input('email? ')
 
-document.add_paragraph('hello')
+document.add_paragraph(
+    name + '|' + phone_number + '|' + email
+)
+
+# about me
+
+document.add_heading('About me')
+document.add_paragraph(input('About yourself? '))
 
 
 document.save('cv.docx')
