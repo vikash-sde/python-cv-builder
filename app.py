406c31e4f0b5a409d3b22ec2c871f4b12420cf13
Vikash Gupta <vikashg2512@gmail.com>
2022-05-01 00:29:29 +0530

    updated code !!

--- app.py
+++ app.py
@@ -20,5 +20,37 @@ document.add_paragraph(
 document.add_heading('About me')
 document.add_paragraph(input('About yourself? '))
 
+# work experience
+document.add_heading('Work experience ')
+p = document.add_paragraph()
+
+company = input('Company ')
+from_date = input('From Date ')
+to_date = input('To Date ')
+
+p.add_run(company + ' - ').bold = True
+p.add_run(from_date + ' - ' + to_date + '\n').italic = True
+
+experience_details = input('Experience Details ' + company)
+p.add_run(experience_details)
+
+# more experiences
+while True:
+    has_more_experiences = input('Do you have more experiences? (yes/no): ').lower()
+    if has_more_experiences == 'yes':
+        p = document.add_paragraph()
+
+        company = input('Company ')
+        from_date = input('From Date ')
+        to_date = input('To Date ')
+
+        p.add_run(company + ' - ').bold = True
+        p.add_run(from_date + ' - ' + to_date + '\n').italic = True
+
+        experience_details = input('Experience Details ' + company)
+        p.add_run(experience_details)
+    else:
+        break
+
 
 document.save('cv.docx')
